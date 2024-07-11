b3b8f40331378840463e4574370f52fbcbffec21
John Kristan <jkpogz09@gmail.com>
2024-07-11 21:36:33 +0800

    added json header in axios request

--- client/src/api/forecast.ts
+++ client/src/api/forecast.ts
@@ -1,6 +1,10 @@
 import axios, { AxiosResponse } from 'axios'
 
 export const getForeCastData = (forecastPeriods: number): Promise<AxiosResponse<any, any>> => {
-    return axios.get(`http://localhost:9090/fetch/forecast/sales?forecast_periods=${forecastPeriods}`);
+    return axios.get(`http://localhost:9090/fetch/forecast/sales?forecast_periods=${forecastPeriods}`, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
 }
 
