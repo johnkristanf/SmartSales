7967d79d4c7892d0f59ee2183f8b15dc87c093ed
John Kristan <jkpogz09@gmail.com>
2024-07-11 21:40:16 +0800

    retrieve and response the monthly sales

--- server/main.py
+++ server/main.py
@@ -1,10 +1,8 @@
 import pandas as pd
-
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from joblib import load
-from train import load_preprocess_sales_dataset, return_monthly_sales
-
+from data import load_preprocess_sales_dataset, return_monthly_sales
 
 app = Flask(__name__)
 CORS(app)
@@ -21,21 +19,25 @@ def load_model_and_forecast(forecast_periods):
 
     forecast_index = pd.date_range(start=monthly_sales.index[-1], periods=forecast_periods, freq='ME')
     forecast_series = pd.Series(forecast_mean, index=forecast_index)
+    forecast_series = forecast_series.dropna()
 
-    return forecast_series
+    return forecast_series, monthly_sales
 
-@app.get("/fetch/forecast/sales")
+
+@app.route("/fetch/forecast/sales")
 def fetch_forecast_sales():    
+    forecast_periods = request.args.get('forecast_periods', type=int)
+    forecast_series, monthly_sales = load_model_and_forecast(forecast_periods)
 
-    forecast_periods = request.args.get('forecast_periods', default=24, type=int)
-    forecast_series = load_model_and_forecast(forecast_periods)
+    historical_data = [{'date': date.strftime('%Y-%m-%d'), 'sales': sales} for date, sales in zip(monthly_sales.index, monthly_sales.values)]
+    forecast_data = [{'date': date.strftime('%Y-%m-%d'), 'sales': sales} for date, sales in zip(forecast_series.index, forecast_series.values)]
 
-    forecast_data = forecast_series.to_dict()
+    all_sales_data = {
+        'historical_data': historical_data,
+        'forecast_data': forecast_data
+    }
 
-    return jsonify(forecast_data)
-
-
-
+    return jsonify(all_sales_data), 200
 
 if __name__ == '__main__':
     app.run(port=9090, debug=True)
