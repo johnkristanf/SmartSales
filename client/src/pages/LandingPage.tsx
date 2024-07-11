b248da37c61ade5af976eb71a9fb38895a7955ca
John Kristan <jkpogz09@gmail.com>
2024-07-11 23:45:10 +0800

    removed  the graphPage component

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -1,6 +1,6 @@
+import { Link } from "react-router-dom"
 import { NavBar } from "../components/navbar"
 import DataAnalysisSteps from "./DAsteps"
-import GraphsPage from "./GraphsPage"
 import HomePage from "./HomePage"
 
 function LandingPage(){
@@ -13,9 +13,25 @@ function LandingPage(){
 
             <DataAnalysisSteps />
 
-            <GraphsPage />
+            <ProcessToGraphs />
         </>
        
+    )
+}
+
+function ProcessToGraphs(){
+    return(
+        <div className="w-full flex justify-center gap-3 pb-10">
+            <Link
+                to="/smartsales/graphs"
+                className="flex items-center gap-2 hover:opacity-75 hover:cursor-pointer"
+            >
+
+                <h1 className="font-semibold text-blue-950 text-4xl">Procced to Graphs</h1>
+                <img src="/arrow_right.png" width={40} height={40} />
+
+            </Link>
+        </div>
     )
 }
 
