e8b54cf13f259857331180c27248310379336fa7
John Kristan <jkpogz09@gmail.com>
2024-07-12 14:57:47 +0800

    python push to production using docker image

--- client/src/api/forecast.ts
+++ client/src/api/forecast.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosResponse } from 'axios'
 
 export const getForeCastData = (forecastPeriods: number): Promise<AxiosResponse<any, any>> => {
-    return axios.get(`http://localhost:9090/fetch/forecast/sales?forecast_periods=${forecastPeriods}`, {
+    return axios.get(`https://smartsales-smartsales.onrender.com/fetch/forecast/sales?forecast_periods=${forecastPeriods}`, {
         headers: {
             'Content-Type': 'application/json'
         }
